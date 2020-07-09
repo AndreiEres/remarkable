a48a27ea4883a6a9478f61a8aa42a317b5f6099f
Andrei Eres <andrei.eres@ya.ru>
2020-07-09 18:46:04 +0400

    Format links in task

--- spec/system/user_visits_a_list_spec.rb
+++ spec/system/user_visits_a_list_spec.rb
@@ -3,24 +3,31 @@
 require "rails_helper"
 
 describe "User visits a list" do
+  let(:list) { create(:list, title: "My First List") }
+
   before do
     driven_by(:rack_test)
   end
 
   it "sees a list title" do
-    list = create(:list, title: "My First List")
-
     visit list_path(list)
 
     expect(page).to have_css "h1", text: "My First List"
   end
 
   it "sees tasks" do
-    list = create(:list, title: "My First List")
     create(:task, text: "My Task", list: list)
 
     visit list_path(list)
 
     expect(page).to have_css "li", text: "My Task"
+  end
+
+  it "sees formatted links" do
+    create(:task, text: "My Task for https://example.com", list: list)
+
+    visit list_path(list)
+
+    expect(page).to have_css "a", text: "https://example.com"
   end
 end
